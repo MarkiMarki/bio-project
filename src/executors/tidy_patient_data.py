6b8e4a7ffa4286acbb46471b3ba72a051462a25c
TomDav28 <54274672+TomDav28@users.noreply.github.com>
2019-09-15 14:25:35 +0300

    tomer's first for the day

--- src/executors/tidy_patient_data.py
+++ src/executors/tidy_patient_data.py
@@ -1,11 +1,8 @@
 from src.resources.tidying_microscopy import *
 from src.settings.base_settings import *
-from src.resources.qnap_helper import *
 
 if __name__ == "__main__":
-    qnap = QnapHelper()
-    True
-    # patient_codes = get_all_patient_codes()
-    # for patient in patient_codes:
-    #     all_data = get_tidy_data(patient)
-    #     all_data.to_csv(TIDY_DATA_DIRECTORY + str(patient)+".csv")
+    patient_codes = get_all_patient_codes()
+    for patient in patient_codes:
+        all_data = get_tidy_data(patient)
+        all_data.to_csv(TIDY_DATA_DIRECTORY + str(patient)+".csv")
--- src/resources/qnap_helper.py
+++ src/resources/qnap_helper.py
@@ -12,4 +12,3 @@ class QnapHelper:
     def __init__(self, ip=QNAP_IP2, port=QNAP_PORT, username=QNAP_USERNAME2, password=QNAP_PASSWORD2):
         self.qnap = QNAPStats(ip, 443, username, password, verify_ssl=False)
         self.qnap.get_volumes()
-        pass
